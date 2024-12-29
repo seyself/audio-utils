f67c5ef0cbba94851bb0ae32a2bfa49a07251a91
Michiyasu Wada <michiyasu@bascule.co.jp>
2024-12-29 18:03:43 +0900

    response status

--- src/azure/getSpeechToken.ts
+++ src/azure/getSpeechToken.ts
@@ -11,7 +11,7 @@ export async function getSpeechToken(options?: GetSpeechTokenOptions): Promise<s
   const speechRegion = options?.region || process.env.AZURE_SPEECH_REGION;
 
   if (!speechKey || !speechRegion || speechKey === 'paste-your-speech-key-here' || speechRegion === 'paste-your-speech-region-here') {
-    return JSON.stringify({ error: 'You forgot to add your speech key or region to the .env file.' });
+    return JSON.stringify({ data: { error: 'You forgot to add your speech key or region to the .env file.' }, status: 400 });
   }
 
   const headers = { 
@@ -27,8 +27,8 @@ export async function getSpeechToken(options?: GetSpeechTokenOptions): Promise<s
       url = `${options.endpoint}sts/v1.0/issueToken`;
     }
     const tokenResponse = await axios.post(url, null, headers);
-    return JSON.stringify({ token: tokenResponse.data, region: speechRegion });
+    return JSON.stringify({ data: { token: tokenResponse.data, region: speechRegion }, status: 200 });
   } catch (err) {
-    return JSON.stringify({ error: 'There was an error authorizing your speech key.' });
+    return JSON.stringify({ data: { error: 'There was an error authorizing your speech key.' }, status: 401 });
   }
 }
